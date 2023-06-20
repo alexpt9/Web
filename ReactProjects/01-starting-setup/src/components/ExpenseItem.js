d24f6b632b32bdaee6cf266f36b3f09f4fed99d9
Sumit Kumar <sumit.kumar198727@gmail.com>
2023-06-20 20:57:48 +0530

    Learning_20062023

--- ReactProjects/01-starting-setup/src/components/ExpenseItem.js
+++ ReactProjects/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insuranse";
+  const expenseAmount = "294.50";
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
