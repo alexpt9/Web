a78c496b2751fe7ae9abb75dc6a806f0a0fbf5ed
Alex <ptalex92@gmail.com>
2023-06-21 17:25:20 +0530

    learning_21062023

--- ReactProjects/01-starting-setup/src/components/ExpenseItem.js
+++ ReactProjects/01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insuranse";
-  const expenseAmount = "294.50";
+function ExpenseItem(props) {
+  
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.expense.date}/>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.expense.title}</h2>
+        <div className="expense-item__price">{props.expense.amount}</div>
       </div>
     </div>
   );
