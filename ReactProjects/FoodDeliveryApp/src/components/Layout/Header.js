aecc8e10921488eb52cc0c04e551e8ec88e08ef5
Sumit Kumar <sumit.kumar198727@gmail.com>
2023-08-03 21:50:36 +0530

    Added food delivery app

---
+++ ReactProjects/FoodDeliveryApp/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+
+import classes from './Header.module.css';
+import mealsImg from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImg} alt="Table full of delecious food." />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
